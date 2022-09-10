33d407c509abab14e0ea918f5144e85defb592c1
Paulo Silas <paulo3356@gmail.com>
2022-09-10 14:36:48 -0300

    teste de busca para o projeto

--- data.js
+++ data.js
@@ -1,56 +1,56 @@
 let database = [
     {
         nome: "Restaurante",
-        descricao: "Restaurante 1 Restaurante 1 Restaurante1  Restaurante 1",
+        descricao: "Restaurante e Lanchonete em Belo Horizonte",
         img: "<img src='./imagens/restaurante1.jpg'>",
         tags:['restaurante', 'lanchonete'],
         cidade: ['Belo Horizonte', 'belo horizonte', 'belo']
     },
     {
         nome: "Restaurante 2",
-        descricao: "Restaurante 2 Restaurante 2 Restaurante 2 Restaurante 2",
+        descricao: "Restaurante em Belo Horizonte",
         img: "<img src='./imagens/restaurante2.jpg'>",
         tags:['lanchonete', 'restaurante', 'padaria'],
         cidade: ['Belo Horizonte', 'belo horizonte', 'belo']
     },
     {
         nome: "PetShop1",
-        descricao: "PetShop1 PetShop1 PetShop1 PetShop1",
+        descricao: "PetShop1 em São Paulo",
         img: "<img src='./imagens/PetShop.jpg'>",
         tags:['pet', 'petshop'],
         cidade: ['sao paulo', 'São Paulo', 'são', 'sao']
     },
     {
         nome: "Marcenaria, Carpintaria",
-        descricao: "Marcenaria Marcenaria Marcenaria Marcenaria",
+        descricao: "Marcenaria em Belo Horizonte",
         img: "<img src='./imagens/artesao.jpg'>",
         tags:['Marcenaria','marcenaria', 'moveis', 'Carpintaria','carpintaria','carpinteiro'],
         cidade: ['Belo Horizonte', 'belo horizonte', 'belo']
     },
     {
         nome: "PetShop2",
-        descricao: "PetShop2 PetShop2 PetShop2 PetShop2",
+        descricao: "PetShop2 em São Paulo",
         img: "<img src='./imagens/PetShop2.png'>",
         tags:['pet', 'petshop'],
         cidade: ['sao paulo', 'São Paulo', 'são', 'sao']
     },
     {
         nome: "PetShop3",
-        descricao: "PetShop PetShop PetShop PetShop",
+        descricao: "PetShop em Belo Horizonte e São Paulo",
         img: "<img src='./imagens/PetShop.jpg'>",
         tags:['pet', 'petshop'],
         cidade: ['belo', 'Belo Horizonte', 'São Paulo', 'são', 'sao']
     },
     {
         nome: "Restaurante 3",
-        descricao: "Restaurante 3 Restaurante 3 Restaurante 3  Restaurante 3",
+        descricao: "Restaurante em São Paulo",
         img: "<img src='./imagens/restaurante1.jpg'>",
         tags:['restaurante', 'lanchonete'],
         cidade: ['sao paulo', 'São Paulo', 'são', 'sao']
     },
     {
         nome: "Marcenaria, Carpintaria",
-        descricao: "Marcenaria Marcenaria Marcenaria Marcenaria",
+        descricao: "Carpintaria em São Paulo ",
         img: "<img src='./imagens/marcenaria.jfif'>",
         tags:['Marcenaria','marcenaria', 'moveis', 'Carpintaria','carpintaria','carpinteiro'],
         cidade: ['sao paulo', 'São Paulo', 'são', 'sao']
